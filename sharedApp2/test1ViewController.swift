6ca62e6161bc41c1ff3c25783dddfb217b4611f2
mgoodling15 <floam5425>
2018-01-27 17:28:44 -0500

    god bless america

---
+++ sharedApp2/test1ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  sharedApp2
+//
+//  Created by Megan Goodling on 1/27/18.
+//  Copyright © 2018 Megan Goodling. All rights reserved.
+//
+
+import UIKit
+
+class test1ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        performSegue(withIdentifier: "1to2", sender: self)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
