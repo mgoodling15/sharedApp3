7d5a1d710faa0cbea23e39a071e4708922c4c250
Sarah <sarah.k.overton@gmail.com>
2018-01-27 22:25:16 -0500

    STARS DO THEIR RATING THING BOIS

---
+++ sharedApp2/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  sharedApp2
+//
+//  Created by Sarah Overton on 1/27/18.
+//  Copyright © 2018 Megan Goodling. All rights reserved.
+//
+
+import UIKit
+
+class Profile: UIStackView {
+
+   
+
+}
