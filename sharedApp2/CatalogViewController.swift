fed417424aeb565f40676b9360be8dfdb7e351e7
Sarah <sarah.k.overton@gmail.com>
2018-01-27 20:23:29 -0500

    Added a new .swift file for the catalog transitions

---
+++ sharedApp2/CatalogViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  sharedApp2
+//
+//  Created by Megan Goodling on 1/27/18.
+//  Copyright © 2018 Megan Goodling. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
