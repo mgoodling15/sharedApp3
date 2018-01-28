920f36f019e294eb92b664f93d558dd1f2ccfaef
Sarah <sarah.k.overton@gmail.com>
2018-01-27 20:56:39 -0500

    Added red button to profile - will become star rating

--- sharedApp2/RatingControl.swift
+++ sharedApp2/RatingControl.swift
@@ -9,13 +9,39 @@
 import UIKit
 
 class RatingControl: UIStackView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
     }
-    */
-
+    
+    
+    required init(coder: NSCoder){
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton){
+        print("Button pressed")
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons(){
+        // Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self, action: #selector (RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+    }
+    
 }
