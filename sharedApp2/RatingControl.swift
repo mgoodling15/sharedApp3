275033171655a03c285e5296e75274f99e33aa61
Sarah <sarah.k.overton@gmail.com>
2018-01-27 20:31:09 -0500

    Adding star ratings

---
+++ sharedApp2/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  sharedApp2
+//
+//  Created by Sarah Overton on 1/27/18.
+//  Copyright © 2018 Megan Goodling. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
